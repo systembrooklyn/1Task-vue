ecd7b74dc61037a2bdb331e890d9bd51ef5ccf85
systembrooklyn <system.bbs.2024@gmail.com>
2024-12-22 16:20:37 +0200

    fixed:   src/main.js fixed:   vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,3 @@
 module.exports = {
   publicPath: "/", // غير "my-app" للمسار الفرعي اللي محتاجه، أو خليها '/' لو على المسار الرئيسي
 };
-
-// vite.config.js
-export default {
-  build: {
-    minify: "esbuild", // أو terser للحصول على حماية أقوى
-  },
-};
